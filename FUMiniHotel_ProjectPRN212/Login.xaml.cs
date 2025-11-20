c5d16fffde9115201e0f3d0df42c3f4b89d50afb
Xuntacdor <nhatquang1223@gmail.com>
2025-11-20 20:33:22 +0700

    init  WPF app

---
+++ FUMiniHotel_ProjectPRN212/Login.xaml.cs
@@ -0,0 +1,100 @@
+﻿using BusinessObjects;
+using FUMiniHotel_ProjectPRN212.Admin;
+using FUMiniHotel_ProjectPRN212.Customer;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FUMiniHotel_ProjectPRN212
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        private readonly IUserService userService;
+        private readonly ICustomerService customerService;
+        public Login()
+        {
+            customerService = new CustomerService();
+            InitializeComponent();
+            userService = new UserService();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text;
+            string password = txtPass.Password;
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Vui lòng nhập email và mật khẩu!", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            bool loginResult = userService.Login(email, password);
+            if (loginResult) 
+            {
+                //ktra xem co trung voi tk cua admin khong
+                bool isAdmin = userService.IsAdmin(email);
+                if (isAdmin) 
+                {
+                    MessageBox.Show("Hello Admin!, Welcome to FU Mini Hotel Management", "Login Successful", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Hide();
+                    AdminDashboard adminDashboard = new AdminDashboard();   
+                    adminDashboard.Show();
+                    this.Close();
+                }
+                var customer = customerService.GetCustomerByEmail(email);
+                if (customer != null) 
+                {
+                    MessageBox.Show($"Hello {customer.FullName} , Welcome to FU Mini Hotel Management", "Login Successful", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Hide();
+                    CustomerDashboard customerDashboard = new CustomerDashboard(customer.CustomerId);
+                    customerDashboard.Show();
+                    this.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Email hoặc mật khẩu không đúng!", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            Register register = new Register();
+            register.Show();
+            this.Close();
+        }
+
+        private void btnForgotPassword_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+    }
+}
---
+++ FUMiniHotel_ProjectPRN212/Register.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FUMiniHotel_ProjectPRN212
+{
+    /// <summary>
+    /// Interaction logic for Register.xaml
+    /// </summary>
+    public partial class Register : Window
+    {
+        private readonly IUserService userService;
+        public Register()
+        {
+            InitializeComponent();
+            userService = new UserService();    
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            string email = txtEmail.Text;
+            string fullname = txtFullName.Text;
+            string telephone = txtTelephone.Text;
+            string password = txtPassword.Password;
+            string confirmPassword = txtConfirmPassword.Password;
+            DateTime? birthday = dpBirthday.SelectedDate;
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(fullname) || string.IsNullOrEmpty(telephone) || string.IsNullOrEmpty(password)
+                || (string.IsNullOrEmpty(confirmPassword) || birthday == null)) 
+            {
+                MessageBox.Show("Vui lòng điền đầy đủ thông tin !", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (password != confirmPassword) 
+            {
+                MessageBox.Show("Mật khẩu và xác nhận mật khẩu không giống nhau !", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (userService.CheckEmailExists(email))
+            {
+                MessageBox.Show("Email đã tồn tại!", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            bool registerResult = userService.Register(email,"Customer","Active", fullname, telephone, password,birthday);
+            if (registerResult) 
+            {
+                MessageBox.Show("Đăng ký thành công!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+                Login login = new Login();
+                login.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Đăng ký thất bại!", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnBackToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Close();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
